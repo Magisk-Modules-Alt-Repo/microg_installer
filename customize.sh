735fcfb79433d3655f4cf6fde7793d9f5930a410
nift4 <nift4@protonmail.com>
2022-02-09 08:55:20 +0100

    fix minor logic bug
    Change-Id: Ie8917851a2f3bf4ba44f4c76720e1ad103d71cdc

--- customize.sh
+++ customize.sh
@@ -3,7 +3,6 @@ if $BOOTMODE; then
 pm install --dont-kill -g "$MODPATH/system/priv-app/GmsCore/GmsCore.apk"
 pm install --dont-kill "$MODPATH/SigSpoofHelper.apk" 2>/dev/null
 rm -f "$MODPATH/SigSpoofHelper.apk"
-pm grant com.android.vending android.permission.FAKE_PACKAGE_SIGNATURE
 if [ -f /data/adb/Phonesky.apk ]; then
 ui_print "- Installing real Play Store"
 cp /data/adb/Phonesky.apk "$MODPATH/system/priv-app/Phonesky/Phonesky.apk"
@@ -11,6 +10,8 @@ else
 ui_print "- Real Play Store not found, installing Fake Store"
 fi
 pm install --dont-kill "$MODPATH/system/priv-app/Phonesky/Phonesky.apk"
+pm grant com.android.vending android.permission.FAKE_PACKAGE_SIGNATURE
+
 else
 ui_print "- ERROR: Installation via recovery is NOT supported."
 exit 1
