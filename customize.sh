819b0ab0073cd8883819a3de4452d04d053cc296
Fs00 <francescosaltori@gmail.com>
2022-03-17 19:24:38 +0100

    Don't reinstall real Play Store if already installed

--- customize.sh
+++ customize.sh
@@ -1,18 +1,30 @@
+install_phonesky()
+{
+    pm install --dont-kill "$MODPATH/system/priv-app/Phonesky/Phonesky.apk"
+    pm grant com.android.vending android.permission.FAKE_PACKAGE_SIGNATURE
+}
+
 if $BOOTMODE; then
 
-pm install --dont-kill -g "$MODPATH/system/priv-app/GmsCore/GmsCore.apk"
-pm install --dont-kill "$MODPATH/SigSpoofHelper.apk" 2>/dev/null
-rm -f "$MODPATH/SigSpoofHelper.apk"
-if [ -f /data/adb/Phonesky.apk ]; then
-ui_print "- Installing real Play Store"
-cp /data/adb/Phonesky.apk "$MODPATH/system/priv-app/Phonesky/Phonesky.apk"
-else
-ui_print "- Real Play Store not found, installing Fake Store"
-fi
-pm install --dont-kill "$MODPATH/system/priv-app/Phonesky/Phonesky.apk"
-pm grant com.android.vending android.permission.FAKE_PACKAGE_SIGNATURE
+    pm install --dont-kill -g "$MODPATH/system/priv-app/GmsCore/GmsCore.apk"
+    pm install --dont-kill "$MODPATH/SigSpoofHelper.apk" 2>/dev/null
+    rm -f "$MODPATH/SigSpoofHelper.apk"
+    if [ -f /data/adb/Phonesky.apk ]; then
+        cp /data/adb/Phonesky.apk "$MODPATH/system/priv-app/Phonesky/Phonesky.apk"
+        # If the real Play Store is already installed, don't install it again since
+        # it will result in an error if real Play Store is not patched and has
+        # already auto-updated itself
+        if [ ! $(pm list packages | grep com.android.vending) ]; then
+            ui_print "- Installing real Play Store"
+            install_phonesky
+        fi
+    else
+        ui_print "- Real Play Store not found, installing Fake Store"
+        install_phonesky
+    fi
 
 else
-ui_print "- ERROR: Installation via recovery is NOT supported."
-exit 1
+    ui_print "- ERROR: Installation via recovery is NOT supported."
+    exit 1
 fi
+
