8ac8f7beb713bc4d3e54f2fa062f9c53445cab81
nift4 <nift4@protonmail.com>
2023-05-05 17:06:02 +0200

    Release 3.3.2-0

--- customize.sh
+++ customize.sh
@@ -7,9 +7,14 @@ else
   mmm_exec() { true; }
 fi
 
-session_installer_supported=false
+if ! $BOOTMODE; then
+    abort "- ERROR: Installation via recovery is NOT supported."
+fi
+mmm_exec setSupportLink "https://github.com/nift4/microg_installer_revived/issues"
+session_installer_supported=true
 
 pm_install() {
+    mmm_exec showLoading
     newretval=1
     tmplocapk=/data/local/tmp/microg_revived_TMP_DELETE_ME.apk
     cp "$2" "$tmplocapk"
@@ -19,7 +24,7 @@ pm_install() {
         if [ $? -eq 0 ]; then
             session=$(echo "$sessionRaw" | sed -E 's/.*\[(.*)\].*/\1/g')
             if [ $? -eq 0 ]; then
-                pm install-write "$session" "$tmplocapk"
+                pm install-write "$session" base "$tmplocapk"
                 if [ $? -eq 0 ]; then
                     pm install-commit "$session"
                     newretval=$?
@@ -35,40 +40,33 @@ pm_install() {
         newretval=$?
     fi
     rm "$tmplocapk"
+    mmm_exec hideLoading
     return $newretval
 }
 
-install_phonesky() {
-    mmm_exec showLoading
-    pm_install "" "$MODPATH/system/priv-app/Phonesky/Phonesky.apk"
-    pm grant com.android.vending android.permission.FAKE_PACKAGE_SIGNATURE 2>/dev/null
-    mmm_exec hideLoading
-}
-
-if $BOOTMODE; then
-    mmm_exec setSupportLink "https://github.com/nift4/microg_installer_revived/issues"
-
-    ui_print "- Installing microG GmsCore"
-    mmm_exec showLoading
-    pm_install "-g" "$MODPATH/system/priv-app/GmsCore/GmsCore.apk"
-    pm grant com.google.android.gms android.permission.FAKE_PACKAGE_SIGNATURE 2>/dev/null
-    mmm_exec hideLoading
-
-    if [ -f /data/adb/Phonesky.apk ]; then
-        ui_print "- Installing real Play Store"
-        cp /data/adb/Phonesky.apk "$MODPATH/system/priv-app/Phonesky/Phonesky.apk"
-        # If the real Play Store is already installed, don't install it again since
-        # it will result in an error if real Play Store is not patched and has
-        # already auto-updated itself
-        if [ ! $(pm list packages | grep com.android.vending) ]; then
-            install_phonesky
-        fi
-    else
-        ui_print "- Real Play Store not found, installing Fake Store"
-        install_phonesky
-    fi
-
+if [ -f /data/adb/Phonesky.apk ]; then
+    ui_print "- Found real Play Store"
+    real_ps=true
+    cp /data/adb/Phonesky.apk "$MODPATH/system/priv-app/Phonesky/Phonesky.apk"
 else
-    abort "- ERROR: Installation via recovery is NOT supported."
+    real_ps=false
+    ui_print "- Real Play Store not found, using Fake Store"
 fi
 
+ui_print "- Installing microG GmsCore"
+pm_install "-g" "$MODPATH/system/priv-app/GmsCore/GmsCore.apk"
+
+if $real_ps; then
+    ui_print "- Installing real Play Store"
+else
+    ui_print "- Real Play Store not found, installing Fake Store"
+fi
+# If the real Play Store is already installed, don't install it again since
+# it will result in an error if real Play Store is not patched and has
+# already auto-updated itself
+if [ ! $(pm list packages | grep com.android.vending) ]; then
+    pm_install "" "$MODPATH/system/priv-app/Phonesky/Phonesky.apk"
+fi
+
+pm grant com.google.android.gms android.permission.FAKE_PACKAGE_SIGNATURE 2>/dev/null
+pm grant com.android.vending android.permission.FAKE_PACKAGE_SIGNATURE 2>/dev/null
